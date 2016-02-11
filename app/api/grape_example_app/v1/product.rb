8f782e748e23cc28da9f0db57934ad098a92a61c
Johnny Gomez <jchengnc@gmail.com>
2016-02-11 19:10:00 +0000

    Implemented product and review backend.

--- app/api/grape_example_app/v1/product.rb
+++ app/api/grape_example_app/v1/product.rb
@@ -1,4 +1,100 @@
 module GrapeExampleApp
   class V1::Product < Grape::API
+    use Rack::JSONP
+
+    helpers do
+      params :token do
+        optional :token, type: String, default: nil
+      end
+
+      params :attributes do
+        optional :attributes, type: Hash, default: {}
+      end
+    end
+
+    resource :products do
+      # /products
+      get do
+        api_response(::Product.all.to_json)
+      end
+
+      # /product/:id
+      route_param :id do
+        params do
+          use :token, type: String, desc: 'Authentication token'
+          requires :id, type: Integer, desc: "Product ID"
+        end
+        get do
+          begin
+            authenticate!
+
+            product = ::Product.find(params[:id])
+            api_response(product.to_json)
+          rescue ActiveRecord::RecordNotFound => e
+            status 404 # Not Found
+          end
+        end
+      end
+
+      # POST /products {:attributes}
+      params do
+        use :token
+        requires :attributes, type: Hash, desc: 'Product object to create' do
+          requires :name, type: String, desc: 'Name of product'
+          requires :description, type: String, desc: 'Description of product'
+          requires :image_url, type: String, desc: 'URL of image for product'
+          requires :price, type: Integer, desc: 'Price of product'
+          requires :stock, type: Integer, desc: 'Stock of product'
+        end
+      end
+      post do
+        begin
+            authenticate!
+            safe_params = clean_params(params[:attributes])
+                            .permit(:name, :description, :image_url, :price, :stock)
+    
+            if safe_params
+                ::Product.create!(safe_params)
+                status 200 # Saved OK
+            end
+        rescue ActiveRecord::RecordNotFound => e
+            status 404 # Not Found
+        end
+      end
+
+      # PUT /product {:attributes}
+      params do
+        use :token, type: String, desc: 'Authentication token'
+        requires :id, type: Integer, desc: "Product ID"
+        requires :attributes, type: Hash, desc: 'Product object to create' do
+          requires :name, type: String, desc: 'Name of product'
+          requires :description, type: String, desc: 'Description of product'
+          requires :image_url, type: String, desc: 'URL of image for product'
+          requires :price, type: Integer, desc: 'Price of product'
+          requires :stock, type: Integer, desc: 'Stock of product'
+        end
+      end
+      put ':id' do
+        begin
+          authenticate!
+          safe_params = clean_params(params[:attributes]).permit(:name, :description, :image_url, :price, :stock)
+
+          if safe_params
+            product = ::Product.find(params[:id])
+            product.update_attributes(safe_params)
+            status 200 # Saved OK
+          end
+        rescue ActiveRecord::RecordNotFound => e
+          status 404 # Not found
+        end
+      end
+
+
+
+
+    # end of resource
+    end
+
+
   end
 end
--- app/api/grape_example_app/v1/review.rb
+++ app/api/grape_example_app/v1/review.rb
@@ -1,4 +1,86 @@
 module GrapeExampleApp
   class V1::Review < Grape::API
+    use Rack::JSONP
+
+    helpers do
+      params :token do
+        optional :token, type: String, default: nil
+      end
+
+      params :attributes do
+        optional :attributes, type: Hash, default: {}
+      end
+    end
+
+    resource :reviews do
+        # get
+        get do
+            api_response(::Review.all.to_json)
+        end
+
+        # get by id
+        route_param :id do
+            params do
+                use :token, type: String, desc: 'Authentication token'
+                requires :id, type: Integer, desc: "Review ID"
+            end
+            get do
+                begin
+                  authenticate!
+
+                  review = ::Review.find(params[:id])
+                  api_response(review.to_json)
+                rescue ActiveRecord::RecordNotFound => e
+                    status 404 # Not found
+                end
+            end
+        end
+
+        # POST review
+        params do
+            use :token
+            requires :attributes, type: Hash, desc: 'Review object to create' do
+              requires :title, type: String, desc: 'Title of review'
+              requires :body, type: String, desc: 'Body of review'
+            end
+        end
+        post do
+            begin
+                authenticate!
+                safe_params = clean_params(params[:attributes]).permit(:title, :body)
+
+                if safe_params
+                  ::Review.create!(safe_params)
+                  status 200 # Saved OK
+                end
+            rescue ActiveRecord::RecordNotFound => e
+                status 404 # Not found
+            end
+        end
+        # PUT review by id
+        params do
+            use :token, type: String, desc: 'Authentication token'
+            requires :id, type: Integer, desc: "Review ID"
+            requires :attributes, type: Hash, desc: 'Review object to create' do
+              requires :title, type: String, desc: 'Title of review'
+              requires :body, type: String, desc: 'Body of review'
+            end
+        end
+        put ':id' do
+            begin
+              authenticate!
+              safe_params = clean_params(params[:attributes]).permit(:title, :body)
+
+              if safe_params
+                review = ::Review.find(params[:id])
+                review.update_attributes(safe_params)
+                status 200 # Saved OK
+              end
+            rescue ActiveRecord::RecordNotFound => e
+              status 404 # Not found
+            end
+        end
+    # end of resource
+    end
   end
 end
